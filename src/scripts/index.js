ab7eca40bbeea31c29392d5c68e8b525007b7d2c
DeniJuliSetiawan112 <denijuli.setiawan@gmail.com>
2020-09-06 09:21:27 +0700

    resize asset

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,10 +5,28 @@ import '../styles/responsive.css';
 console.log('Hello Coders! :)');
 
 
-const menuButton = document.querySelector('#menu');
+const menuButton = document.querySelector('#menu')
 const navbarElement = document.querySelector('.navbar')
+const mainElement = document.querySelector('main')
+const HeroElement = document.querySelector('.hero')
+const FooterElement = document.querySelector('footer')
 
 menuButton.addEventListener('click', event => {
+    navbarElement.classList.toggle('open')
+    event.stopPropagation()
+})
+
+mainElement.addEventListener('click', event => {
+    navbarElement.classList.toggle('open')
+    event.stopPropagation()
+})
+
+HeroElement.addEventListener('click', event => {
+    navbarElement.classList.toggle('open')
+    event.stopPropagation()
+})
+
+FooterElement.addEventListener('click', event => {
     navbarElement.classList.toggle('open')
     event.stopPropagation()
 })
