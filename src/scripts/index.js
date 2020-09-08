c27b9ed2a9ef9d6f7a31be2f0511be23c96b7bc1
DeniJuliSetiawan112 <denijuli.setiawan@gmail.com>
2020-09-08 19:34:56 +0700

    fix submission

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
+import data from '../DATA.json';
 
 console.log('Hello Coders! :)');
 
@@ -17,16 +18,43 @@ menuButton.addEventListener('click', event => {
 })
 
 mainElement.addEventListener('click', event => {
-    navbarElement.classList.toggle('open')
+    navbarElement.classList.remove('open')
     event.stopPropagation()
 })
 
 HeroElement.addEventListener('click', event => {
-    navbarElement.classList.toggle('open')
+    navbarElement.classList.remove('open')
     event.stopPropagation()
 })
 
 FooterElement.addEventListener('click', event => {
-    navbarElement.classList.toggle('open')
+    navbarElement.classList.remove('open')
     event.stopPropagation()
 })
+
+
+ 
+let restaurantElement = '';
+data.restaurants.forEach(resto => {
+    restaurantElement += `
+                <article class="post__item">
+                    <div class="post__header">
+                        <div class="post__kota">
+                            <p>${resto.city}</p>
+                        </div>
+                        <div class="post__thumbnail">
+                            <img src="${resto.pictureId}" alt="${resto.name}">
+                        </div>
+                    </div>
+                    <div class="post__body">
+                        <div class="rating">
+                            <img src="./images/icon/star.svg" alt="rating">
+                            <p>${resto.rating}</p>
+                        </div>
+                        <a href="#" class="post__title">${resto.name}</a>
+                        <p class="post__description">${resto.description.substr(0,160)}</p>
+                    </div>
+                </article>
+    `            
+});
+document.querySelector("#post__article").innerHTML = restaurantElement;
